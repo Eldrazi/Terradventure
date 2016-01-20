c851b001a4f06dbda6ea86a1c63d894f13f61a2f
Eldrazi <robkuijper0703@gmail.com>
2016-01-20 23:00:55 +0100

    Initial commit

---
+++ Terradventure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Terraria.ModLoader;
+
+namespace Terradventure
+{
+    public class Terradventure : Mod
+    {
+        public override void SetModInfo(out string name, ref ModProperties properties)
+        {
+            name = "Terradventure";
+            properties.Autoload = true;
+        }
+    }
+}
